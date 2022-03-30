ac8073ac83fd54b96f7db4fcbcce801c10cd342b
Hadi Ansari <hadan326@student.liu.se>
2022-03-30 22:20:22 +0200

    Added a header and some styles to the buttons in start screen

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity} from 'react-native';
 
 export default function App() {
+
+  const cityBtnClickHandler = () => {
+    console.log("City button clicked!");
+  }
+
+  const countryBtnClickHandler = () => {
+    console.log("Country button clicked!");
+  }
+
   return (
-    <View style={styles.container}>
-      <Button title = "SEARCH BY CITY" />
-      <Button title = "SEARCH BY COUNTRY" />
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>CityPop</Text>
+
+      <View style={styles.buttonContainer}>
+          <Button
+            onPress={cityBtnClickHandler}
+            title="SEARCH BY CITY"
+            color="#6119e6"/>
+      </View>
+
+      <View style={styles.buttonContainer}>
+          <Button
+            onPress={countryBtnClickHandler}
+            title="SEARCH BY COUNTRY"
+            color="#6119e6"/>
+      </View> 
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,5 +40,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    marginBottom: 100,
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  buttonContainer:{
+    width: "60%",
+    margin: 10,
   },
 });
