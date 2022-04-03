6d217a4a9c5072f4ff7b6e317aabc6ffc455b9c5
Hadi Ansari <hadan326@student.liu.se>
2022-04-03 19:45:03 +0200

    working with modularity (created CityItemComponent)

---
+++ app/component/CityItemComponent.js
@@ -0,0 +1,43 @@
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+export default function CityItemComponent( props ) {
+  cityClickHandler = (cityName, cityPopulation) => {
+
+    props.navigation.navigate("cityResultScreen", { cityName: cityName.toUpperCase(), cityPopulation })
+   
+  }
+
+  return ( 
+    <TouchableOpacity style={styles.cityItemView} onPress = {() => cityClickHandler(props.cityName, props.cityPopulation)}>
+      <View>
+        <Text style={{ fontSize: 25 }}>
+          {props.cityName}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    marginBottom: 100,
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  cityItemView: {
+    height: 50,
+    width: '80%',
+    marginBottom: 5,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
--- app/screens/CountryResultScreen.js
+++ app/screens/CountryResultScreen.js
@@ -1,38 +1,18 @@
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import dataGetter from '../utils/dataGetter'
+import CityItemComponent from '../component/CityItemComponent'
 
 export default function CountryResultScreen({ navigation, route }) {
-  cityClickHandler = (cityName) => {
-    console.log(cityName, "clicked")
-    
-
-    dataGetter.fetchCityPopulation(cityName).then(cityPopulation => {
-      navigation.navigate("cityResultScreen", { cityName: cityName.toUpperCase(), cityPopulation })
-    }).catch(() => {
-      console.log("No result")
-    })
-  }
-
-  let cityItems = []
-
-  for (let i = 0; i < route.params.mostPopulatedCities.length; i++) {
-    cityItems.push(
-      <TouchableOpacity style={styles.cityView} key={i} onPress={() => cityClickHandler(route.params.mostPopulatedCities[i].cityName)}>
-        <View>
-          <Text style={{ fontSize: 25 }}>
-            {route.params.mostPopulatedCities[i].cityName}
-          </Text>
-        </View>
-      </TouchableOpacity>
-
-    )
-  }
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.header}>{route.params.countryName}</Text>
-      {cityItems}
+      {
+        route.params.mostPopulatedCities.map((city, key) => {
+          return <CityItemComponent key = {key} cityName={city.cityName} cityPopulation = {city.cityPopulation} navigation = {navigation}/>
+        })
+      }
     </SafeAreaView>
   )
 }
