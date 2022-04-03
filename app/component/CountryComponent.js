bdff743280321abd746dfd0dbe7ab0a6ecc861ed
Hadi Ansari <hadan326@student.liu.se>
2022-04-03 20:41:55 +0200

    Working with modularity (created CountryComponent)

---
+++ app/component/CountryComponent.js
@@ -0,0 +1,31 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+import CityItemComponent from './CityItemComponent'
+
+export default function CountryComponent( props ) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>{ props.countryName }</Text>
+            {
+                props.mostPopulatedCities.map((city, key) => {
+                    return <CityItemComponent key={key} cityName={city.cityName} cityPopulation={city.cityPopulation} navigation={props.navigation} />
+                })
+            }
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%'
+    },
+    header: {
+        marginBottom: 100,
+        fontSize: 30,
+        fontWeight: 'bold'
+    }
+})
--- app/screens/CountryResultScreen.js
+++ app/screens/CountryResultScreen.js
@@ -1,41 +1,18 @@
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
-import dataGetter from '../utils/dataGetter'
-import CityItemComponent from '../component/CityItemComponent'
+import CountryComponent from '../component/CountryComponent'
 
 export default function CountryResultScreen({ navigation, route }) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.header}>{route.params.countryName}</Text>
-      {
-        route.params.mostPopulatedCities.map((city, key) => {
-          return <CityItemComponent key = {key} cityName={city.cityName} cityPopulation = {city.cityPopulation} navigation = {navigation}/>
-        })
-      }
+      <CountryComponent countryName = { route.params.countryName } mostPopulatedCities = { route.params.mostPopulatedCities } navigation = { navigation }/>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    marginBottom: 100,
-    fontSize: 30,
-    fontWeight: 'bold'
-  },
-  cityView: {
-    height: 50,
-    width: '80%',
-    marginBottom: 5,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flex: 1
   }
 })
